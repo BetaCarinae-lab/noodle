962bb8f30eb43c6aa3e011bd1f0516d4e22bf586
Finn Fagan <finn.d.fagan@gmail.com>
2025-12-30 11:56:13 -0700

    Continuing progress on templates, Also reformated actionDictionary

--- src/semantics.js
+++ src/semantics.js
@@ -4,25 +4,32 @@ export const actionDictionary = {
     Program(statements, _semi) {
         return statements.children.map(s => s.eval(this.args.env));
     },
+
     Statement(stmt) {
         return stmt.eval(this.args.env);
     },
+
     Print(_print, _lp, expr, _rp) {
         const value = expr.eval(this.args.env);
         console.log('OUT: ' + value);
     },
+
     Math_add(expr1, _plus, expr2) {
         return expr1.eval(this.args.env) + expr2.eval(this.args.env)
     },
+
     Math_minus(expr1, _minus, expr2) {
         return expr1.eval(this.args.env) - expr2.eval(this.args.env)
     },
+
     Math_times(expr1, _times, expr2) {
         return expr1.eval(this.args.env) * expr2.eval(this.args.env)
     },
+
     Math_divide(expr1, _divide, expr2) {
         return expr1.eval(this.args.env) / expr2.eval(this.args.env)
     },
+
     VarCreate(mut, type, name, _eq, value) {
         if(typeof value.eval(this.args.env) == type.sourceString || (type.sourceString == 'array' && Array.isArray(value.eval(this.args.env)))) {
             this.args.env[name.sourceString] = {
@@ -31,10 +38,10 @@ export const actionDictionary = {
                 mutable: mut.sourceString ? true : false,
             }
         } else {
-            console.log(`Mismatched Types, Expected ${type.sourceString}, Got ${typeof value.eval(this.args.env)}`)
-            throw new Error(get_error_from_code('MISMATCHED_TYPES'))
+            throw new Error(`Mismatched Types, Expected ${type.sourceString}, Got ${typeof value.eval(this.args.env)}`)
         }
     },
+
     VarAssign(name, _eq, value) {
         if(this.args.env[name.sourceString] && this.args.env[name.sourceString].mutable) {
             this.args.env[name.sourceString].value = value
@@ -42,6 +49,7 @@ export const actionDictionary = {
             throw new Error(this.args.env[name.sourceString] ? `That value is not mutable` : `No value found with name ${name.sourceString}`)
         }
     },
+
     VarGet(_ot, name, _ct) {
         if(this.args.env[name.sourceString]) {
              return this.args.env[name.sourceString].value
@@ -49,15 +57,19 @@ export const actionDictionary = {
             throw new Error(`No value found with name: ${name.sourceString}`)
         }
     },
+
     Expr(e) {
         return e.eval(this.args.env);
     },
+
     number(digits) {
         return Number(digits.sourceString);
     },
+
     string(_open, chars, _close) {
         return chars.sourceString;
     },
+
     Fn(typeList, _fn, name, ParameterList, body) {
         ParameterList = ParameterList.eval(this.args.env);
         var functionEnv = this.args.env
@@ -84,9 +96,11 @@ export const actionDictionary = {
             }
         }
     },
+
     Array(_ob, values, _cb) {
         return values.asIteration().children.map(c => c.eval(this.args.env))
     },
+
     ArrayAccess(_ot, id, _at, index, _ct) {
         if(this.args.env[id.sourceString]) {
             return this.args.env[id.sourceString].value[index.eval(this.args.env)]
@@ -94,12 +108,14 @@ export const actionDictionary = {
             throw new Error(`No Value Found with name: ${id.sourceString}`)
         }
     },
+
     Parameter(mut, ident) {
         return {
             mutable: mut.sourceString ? true : false,
             name: ident.sourceString, 
         }
     },
+
     Math_increment(ident, _pp) {
         if(this.args.env[ident.sourceString] && this.args.env[ident.sourceString].mutable) {
             this.args.env[ident.sourceString].value++
@@ -107,6 +123,7 @@ export const actionDictionary = {
             throw new Error(this.args.env[ident.sourceString].mutable ? `No value found with name: ${ident.sourceString}` : 'Value is not mutable!')
         }
     },
+
     Math_decrement(ident, _pp) {
         if(this.args.env[ident.sourceString] && this.args.env[ident.sourceString].mutable) {
             this.args.env[ident.sourceString].value--
@@ -114,6 +131,7 @@ export const actionDictionary = {
             throw new Error(this.args.env[ident.sourceString].mutable ? `No value found with name: ${ident.sourceString}` : 'Value is not mutable!')
         }
     },
+
     FnCall(_os, name, parameterList, _cs) {
         if(this.args.env[name.sourceString]) {
             return this.args.env[name.sourceString].body(parameterList.eval(this.args.env))
@@ -121,25 +139,23 @@ export const actionDictionary = {
             throw new Error(`Cannot find function with name: ${name.sourceString}`)
         }
     },
+
     Return(_out, _op, value, _cp) {
         throw new ReturnSignal(value.eval(this.args.env))
     },
+
     FuncBody(_ob, body, _cb) {
         return body.eval(this.args.env)
     },
-    /**
-     * 
-     * @param {import("ohm-js").Node} _op 
-     * @param {import("ohm-js").Node} listOfParams 
-     * @param {import("ohm-js").Node} _cp 
-     * @returns {Array[string]}
-     */
+
     ParameterList(_op, listOfParams, _cp) {
         return listOfParams.asIteration().children.map(c => c.sourceString)
     },
+
     StatementParameterList(_op, listOfParams, _cp) {
         return listOfParams.asIteration().children.map(c => c.eval(this.args.env))
     },
+
     _iter(...children) {
         return children.map(c => c.eval(this.args.env))
     },
@@ -148,7 +164,71 @@ export const actionDictionary = {
         let contructorArgs = body.eval(this.args.env)
         if(!this.args.env[name.sourceString]) {
             this.args.env[name.sourceString] = (parameters) => {
-                let returnedObject = {}
+                let returnedObject = contructorArgs
+
+
+            }
+        }
+    },
+
+    TemplateConstruction(id, objectBody) {
+        
+    },
+
+    TemplateBody(_ob, properties, _cb) {
+        let returnedObject = {
+            properties: {},
+            methods: {},
+        }
+        properties.children.forEach((property) => {
+            property = property.eval(this.args.env)
+            if(property.property) {
+                console.log('Adding Property ' + property.name + ' to returnedObject')
+                returnedObject.properties[property.name] = property
+            } else {
+                console.log('Adding Method ' + property.name + ' to returnedObject')
+                returnedObject.methods[property.name] = property
+            }
+        })
+
+        console.log(returnedObject)
+
+        return returnedObject
+    },
+
+    Property(mut, _prop, id, _is, type, _) {
+        return {
+            property: true,
+            name: id.sourceString,
+            mutable: mut.sourceString ? true : false,
+            type: type.sourceString
+        }
+    },
+
+    Method(_method, id, _is, paramList, funcBody) {
+        paramList = paramList.eval(this.args.env);
+        var methodEnv = this.args.env
+        return {
+            property: false,
+            name: id.sourceString,
+            body: (parameters) => {
+                parameters.forEach((param, index) => {
+                    methodEnv[paramList[index].replace('mut ', '')] = {
+                        type: typeof param,
+                        mutable: paramList[index].startsWith('mut '),
+                        value: param,
+                    }
+                })
+
+                try {
+                    body.eval(methodEnv)
+                } catch(error) {
+                    if(error instanceof ReturnSignal) {
+                        return error.value
+                    } else {
+                        console.error(error.stack)
+                    }
+                }
             }
         }
     },
