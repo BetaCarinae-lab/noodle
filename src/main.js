105022a58da0329b517e4f264c9beba8efe1fc86
Betacarinae <finn.d.fagan@gmail.com>
2026-01-05 14:23:24 -0700

    Fixed earlier failed commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { readFileSync } from 'fs'
 import path from 'path';
 import { runBowl, runND } from './exec.js';
 import readline from "readline";
+import { newProject } from './create_new_project.js';
+import { VERSION } from './info.js';
+import { MWD } from './info.js';
+import { exit } from 'process';
 
 let env = {}
 
@@ -16,6 +20,12 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const file_path = path.isAbsolute(process.argv[2])
+  ? process.argv[2]
+  : path.resolve(process.cwd(), process.argv[2]);
+
+const usingBowl = path.extname(file_path) == ".bowl" ? true : false
+
 if(!process.argv[2]) {
     function repl() {
         console.log('Noodle REPL: ')
@@ -26,17 +36,62 @@ if(!process.argv[2]) {
         })
     }
     repl()
-} else {
+} else if (process.argv[2] == '--new') {
+    newProject(process.argv[3], process.cwd())
+} else if (process.argv[2] == '--info' || process.argv[3] == '--info') {
+    console.log(`
+    ------------------------------------------
+    ### RunInfo:
 
-const inputCode = readFileSync(process.argv[2], 'utf-8');
-const usingBowl = path.extname(process.argv[2]) == ".bowl" ? true : false
+    ## Exec Params
+    UsingBowl: ${usingBowl}
+    Reading: ${file_path}
+    Debug: ${env.secure.debug}
+
+    ## Working Directory and Versions
+    NDV: ${VERSION}
+    MWD: ${MWD}
+    CWD: ${process.cwd()}
+    Trying to Read: ${file_path}
+    NodeV: ${process.version}
+    ------------------------------------------
+    `)
+    exit(0)
+} else if(process.argv[2] == '--help') {
+    console.log(`
+        Usage: noodle [file | options] [options],
+        
+        Examples:
+        noodle -> Opens Noodle REPL
+        noodle --info or noodle ./file.nd --info -> prints run info
+        noodle --help -> prints this
+        noodle ./file.nd or noodle ./file.bowl -> runs a nd or bowl file
+    `)
+    exit(0)
+} 
+else {
+
+
+
+
+const inputCode = readFileSync(file_path, 'utf-8');
 
 console.log(`
-RunInfo:
+------------------------------------------
+### RunInfo:
+
+## Exec Params
 UsingBowl: ${usingBowl}
-Reading: ${process.argv[2]}
+Reading: ${file_path}
 Debug: ${env.secure.debug}
+
+## Working Directory and Versions
+NDV: ${VERSION}
+MWD: ${MWD}
+CWD: ${process.cwd()}
+Trying to Read: ${file_path}
 NodeV: ${process.version}
+------------------------------------------
 `)
 
 
@@ -45,5 +100,7 @@ if(usingBowl) {
 } else {
     runND(inputCode, env)
 }
+
+exit(0)
 
 }
