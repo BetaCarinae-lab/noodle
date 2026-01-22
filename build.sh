7b944037fd61234e0f791ed709768c8429e2cd2d
Betacarinae <finn.d.fagan@gmail.com>
2026-01-21 22:03:28 -0700

    Reverted build.sh

--- build.sh
+++ build.sh
@@ -1,50 +1,8 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -e
 
-echo "====================================="
-echo " Building language + modified Ohm-JS "
-echo "====================================="
-
-ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
-OHM_DIR="$ROOT_DIR/src/ohm"
-
-echo
-echo "→ Building OhmJS"
-
-if [ -d "$OHM_DIR/node_modules" ]; then
-  echo "Ohm has node_modules"
-else
-  cd "$OHM_DIR"
-  npm install
-  echo "-> installed ohm dependencies"
-fi
-
-
-cd "$OHM_DIR"
-npm run build
-cd "$ROOT_DIR"
-sudo mv "$OHM_DIR/dist/ohm.min.js" "$ROOT_DIR/js/ohm.js"
-
-echo
-echo "→ Installing root dependencies"
-npm install
-
-echo
-echo "→ Building TypeScript → JavaScript"
-npm run build
-
-echo
-echo "→ Verifying Ohm-JS output"
-if [ ! -d "$ROOT_DIR/js/ohm" ]; then
-  echo "ERROR: Ohm-JS JS output not found in ./js/ohm"
-  exit 1
-fi
-
-echo
-echo "→ Bundling executable"
 npm run bundle
+npm run build
 
-echo
-echo "====================================="
-echo " Build complete ✅"
-echo "====================================="
+./uninstall.sh
+./install.sh
