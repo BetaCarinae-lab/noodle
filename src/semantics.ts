93268a9e60f38e664cf18d3c067d0c15a8615c66
Betacarinae <finn.d.fagan@gmail.com>
2026-02-04 09:03:41 -0700

    Fixed arrays

--- src/semantics.ts
+++ src/semantics.ts
@@ -333,7 +333,14 @@ export const actionDictionary: ohm.ActionDict<unknown> = {
 
     ArrayAccess(_ot: ohm.Node, id: ohm.Node, _at: ohm.Node, index: ohm.Node, _ct: ohm.Node) {
         if(this.args.env.exists(id.sourceString)) {
-            return this.args.env.get(id.sourceString).get()[index.eval(this.args.env)]
+            console.log(id.eval(this.args.env))
+            let evaled = id.eval(this.args.env)
+            if(typeof evaled == 'string') {
+                console.log('string')
+                return this.args.env.get(evaled).value[index.eval(this.args.env)]
+            } else {
+                return id.eval(this.args.env).value[index.eval(this.args.env)]
+            }
         } else {
             throw new Error(`No Value Found with name: ${id.sourceString}`)
         }
